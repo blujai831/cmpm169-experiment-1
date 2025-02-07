b7245b0c6930a6d6247c16393f45946c7662e4e1
blujai831 <webmaster@blujai831.dev>
2025-02-07 08:59:22 -0800

    Guard against false positives in toggle-local-server.sh

--- bin/toggle-local-server.sh
+++ bin/toggle-local-server.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 if [[ -e log.local/pid ]]; then
     pid=$(<log.local/pid)
-    if ps --pid $pid > /dev/null 2>&1; then
+    if grep cmpm169-experiment-1-server > /dev/null 2>&1 < <(
+        ps a --pid $pid 2>/dev/null | tail -n+2
+    ); then
         kill $pid
         rm -rf log.local
         sleep 1
-        if ps --pid $pid > /dev/null 2>&1; then
-            echo>&2 "server will not stop (pid $pid)"
+        if grep cmpm169-experiment-1-server > /dev/null 2>&1 < <(
+            ps a --pid $pid 2>/dev/null | tail -n+2
+        ); then
+            echo>&2 "server failed to stop (pid $pid)"
             exit 1
         else
             echo "server stopped"
@@ -18,7 +22,8 @@ if [[ -e log.local/pid ]]; then
     fi
 else
     mkdir -p log.local
-    nohup bash -c 'cd ..; python3 -m http.server'`
+    nohup bash -c `
+        `'cd ..; python3 -X cmpm169-experiment-1-server -m http.server'`
         ` > log.local/http.out`
         ` 2> log.local/http.err`
         ` & pid=$!
