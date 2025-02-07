58c1646d642b7eeab24d898c64329b856d78d63f
blujai831 <webmaster@blujai831.dev>
2025-02-07 09:28:55 -0800

    Refactor toggle-local-server.sh

--- bin/toggle-local-server.sh
+++ bin/toggle-local-server.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
-if [[ -e log.local/pid ]]; then
-    pid=$(<log.local/pid)
-    if grep cmpm169-experiment-1-server > /dev/null 2>&1 < <(
-        ps a --pid $pid 2>/dev/null | tail -n+2
-    ); then
-        kill $pid
-        rm -rf log.local
-        sleep 1
-        if grep cmpm169-experiment-1-server > /dev/null 2>&1 < <(
-            ps a --pid $pid 2>/dev/null | tail -n+2
-        ); then
-            echo>&2 "server failed to stop (pid $pid)"
-            exit 1
-        else
-            echo "server stopped"
-        fi
-    else
-        echo "cleaning up stale server pid file"
-        rm -f log.local/pid
-        exec "$BASH_SOURCE"
-    fi
-else
+
+cd "$(dirname "$BASH_SOURCE")"
+
+start-server() {
+    local pid
     mkdir -p log.local
     nohup bash -c `
         `'cd ..; python3 -X cmpm169-experiment-1-server -m http.server'`
@@ -29,15 +12,72 @@ else
         ` & pid=$!
     disown $pid
     echo $pid > log.local/pid
+    echo $pid
+}
+
+server-running() {
+    grep cmpm169-experiment-1-server > /dev/null 2>&1 < <(
+        ps a --pid $1 2>/dev/null | tail -n+2
+    )
+}
+
+try-start-server() {
+    local pid=$(start-server)
     sleep 1
-    if ps --pid $pid > /dev/null 2>&1; then
-        echo "server running (pid $pid)"
+    server-running $pid; return $?
+}
+
+try-stop-server() {
+    if [[ ! -r log.local/pid ]]; then
+        return 3
     else
-        echo>&2 "server failed to start"
-        if [[ -e log.local/http.err ]]; then
-            cat>&2 log.local/http.err
+        local pid=$(<log.local/pid)
+        if server-running $pid; then
+            kill $pid
+            sleep 1
+            if server-running $pid; then
+                return 1
+            else
+                rm -f log.local/pid
+                return 0
+            fi
+        else
+            rm -f log.local/pid
+            return 2
         fi
-        rm -rf log.local
-        exit 1
     fi
-fi
+}
+
+main() {
+    if [[ -r log.local/pid ]]; then
+        try-stop-server; local status=$?
+        case $status in
+        0)  echo "server stopped"
+            return 0 ;;
+        1)  echo>&2 "server failed to stop (pid $(<log.local/pid))"
+            return 1 ;;
+        2)  echo>&2 "pid file was stale; cleaned up, trying again"
+            main; return $? ;;
+        3)  echo>&2 "pid file snatched"
+            return 3 ;;
+        *)  echo>&2 "attempt to stop server gave unknown exit code $status"
+            return $status ;;
+        esac
+    else
+        try-start-server; local status=$?
+        case $status in
+        0)  echo "server started (pid $(<log.local/pid))"
+            return 0 ;;
+        1)  echo>&2 "server failed to start"
+            rm -f log.local/pid
+            if [[ -r log.local/http.err ]]; then
+                cat>&2 log.local/http.err
+            fi
+            return 1 ;;
+        *)  echo>&2 "attempt to start server gave unknown exit code $status"
+            return $status ;;
+        esac
+    fi
+}
+
+main
