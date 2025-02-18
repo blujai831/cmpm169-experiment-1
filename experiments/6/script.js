d0e983f466974a8a648597e4947e04921098bb5b
blujai831 <webmaster@blujai831.dev>
2025-02-18 00:23:29 -0800

    Add finer-grain controls, a model import/export system, and three pretrained models to mess around with

--- experiments/6/script.js
+++ experiments/6/script.js
@@ -6,6 +6,7 @@ class Coroutine {
     // This is my own stackful coroutine implementation.
     // (Admittedly, async makes it easy.)
     // You may recognize it from Experiment 5.
+    // I've improved upon it by making it iterable.
     constructor(callback) {
         this._resume = callback.bind(this);
     }
@@ -67,34 +68,17 @@ class Coroutine {
 
 class Markov {
     constructor() {
-        this._associations = {};
+        this.associations = {};
         this.backtrackLimit = Infinity;
         this.mode = "words";
     }
     reset() {
-        this._associations = {};
-    }
-    internAssociations(what) {
-        let parsed;
-        try {
-            this._associations = JSON.parse(what);
-        } catch (err) {
-            if (err instanceof SyntaxError) {
-                return false;
-            } else {
-                throw err;
-            }
-        }
-        return true;
-    }
-    externAssociations() {
-        return JSON.stringify(this._associations, null, 4);
+        this.associations = {};
     }
     learnSingle(from, to) {
-        console.log(`learn association: ${from} => ${to}`);
-        this._associations[from] ||= {};
-        this._associations[from][to] ||= 0;
-        this._associations[from][to]++;
+        this.associations[from] ||= {};
+        this.associations[from][to] ||= 0;
+        this.associations[from][to]++;
     }
     tokenize(text) {
         if (this.mode == "letters") {
@@ -174,7 +158,7 @@ class Markov {
                     phraseTokens.push(tokens[i]);
                 }
                 const phrase = this.detokenize(phraseTokens);
-                responseWeights = this._associations[phrase];
+                responseWeights = this.associations[phrase];
                 if (responseWeights) {
                     break;
                 } else {
@@ -205,77 +189,282 @@ class Markov {
     }
 }
 
-const markov = new Markov();
-let busy = false;
-let errorState = false;
-const inputField = document.querySelector("input");
-const outputArea = document.querySelector("output");
-const brainArea = document.querySelector("#markov-brain");
-let lastKnownBrain;
-
-async function yieldToPage() {
-    await new Promise(r => setTimeout(r, 0));
-}
-
-inputField.addEventListener("keydown", async (ev) => {
-    if (ev.code == "Enter" && !busy) {
-        if (errorState) {
-            outputArea.innerText = "";
-            errorState = false;
+Markov.DataIO = new class {
+    async compressAndSave(str) {
+        // https://evanhahn.com/javascript-compression-streams-api-with-strings
+        const chunks = [];
+        for await (const chunk of (
+            (new Blob([str])).stream().pipeThrough(
+                new CompressionStream("gzip")
+            )
+        )) {
+            chunks.push(chunk);
         }
-        brainArea.disabled = true;
-        busy = true;
-        const prompt = inputField.value;
-        inputField.value = "";
-        let coro = new Coroutine(async function () {
-            await markov.learn(outputArea.innerText, prompt, this);
-            await this.yield("|||");
-            await markov.respond(prompt, this);
-        });
-        try {
+        // https://stackoverflow.com/a/48280991
+        const link = document.createElement("a");
+        link.setAttribute("href", URL.createObjectURL(
+            new Blob(chunks, {type: "application/gzip"})
+        ));
+        link.setAttribute("download", "markov.gz");
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+    async loadAndDecompress(file) {
+        // https://evanhahn.com/javascript-compression-streams-api-with-strings
+        const chunks = [];
+        for await (const chunk of file.stream().pipeThrough(
+            new DecompressionStream("gzip")
+        )) {
+            chunks.push(chunk);
+        }
+        return (new TextDecoder()).decode(new Uint8Array(
+            await (new Blob(chunks)).arrayBuffer()
+        ));
+    }
+    async fetchAndDecompress(url) {
+        const chunks = [];
+        for await (const chunk of (
+            await (await fetch(url)).blob()
+        ).stream().pipeThrough(
+            new DecompressionStream("gzip")
+        )) {
+            chunks.push(chunk);
+        }
+        return (new TextDecoder()).decode(new Uint8Array(
+            await (new Blob(chunks)).arrayBuffer()
+        ));
+    }
+};
+
+Markov.UI = class {
+    constructor() {
+        this.markov = new Markov();
+        this.busy = false;
+        this.learningMode = "conversational";
+        this.lastPrompt = "";
+        this.promptField = document.querySelector("#prompt");
+        this.responseArea = document.querySelector("#response");
+        this.maxLookbehindField = document.querySelector("#max-lookbehind");
+        this.learningModeSelect =
+            document.querySelector("#learning-mode-select");
+        this.tokenModeSelect = document.querySelector("#token-mode-select");
+        this.exportButton = document.querySelector("#model-export");
+        this.importButton = document.querySelector("#model-import");
+        this.collectionImportButtons =
+            document.querySelectorAll("#collection-import-buttons button");
+        this.registerEventListeners();
+        this.syncModelFromUI();
+    }
+    registerEventListeners() {
+        this.promptField.addEventListener("keydown",
+            this.handlePromptFieldKeydown.bind(this));
+        this.maxLookbehindField.addEventListener("change",
+            this.handleMaxLookbehindFieldChange.bind(this));
+        this.learningModeSelect.addEventListener("change",
+            this.handleLearningModeSelectChange.bind(this));
+        this.tokenModeSelect.addEventListener("change",
+            this.handleTokenModeSelectChange.bind(this));
+        this.exportButton.addEventListener("click",
+            this.handleExportButtonClick.bind(this));
+        this.importButton.addEventListener("change",
+            this.handleImportButtonChange.bind(this));
+        for (const button of this.collectionImportButtons) {
+            button.addEventListener("click",
+                this.handleCollectionImportButtonClick.bind(this, button.id));
+        }
+    }
+    disable() {
+        this.promptField.disabled = true;
+        this.maxLookbehindField.disabled = true;
+        this.learningModeSelect.disabled = true;
+        this.tokenModeSelect.disabled = true;
+        this.exportButton.disabled = true;
+        this.importButton.disabled = true;
+        this.busy = true;
+    }
+    enable() {
+        this.busy = false;
+        this.promptField.disabled = false;
+        this.maxLookbehindField.disabled = false;
+        this.learningModeSelect.disabled = false;
+        this.tokenModeSelect.disabled = false;
+        this.exportButton.disabled = false;
+        this.importButton.disabled = false;
+    }
+    async yieldToPage() {
+        await new Promise(r => setTimeout(r, 0));
+    }
+    handlePromptFieldKeydown(ev) {
+        if (ev.code == "Enter") {
+            this.respond();
+        }
+    }
+    handleMaxLookbehindFieldChange() {
+        if (this.busy) {
+            this.setMaxLookbehindInUIFromModel();
+        } else {
+            this.setMaxLookbehindInModelFromUI();
+        }
+    }
+    setMaxLookbehindInUIFromModel() {
+        const n = this.markov.maxLookbehind;
+        if (isFinite(n)) {
+            this.maxLookbehindField.value = String(n);
+        } else {
+            this.maxLookbehindField.value = String(-1);
+        }
+    }
+    setMaxLookbehindInModelFromUI() {
+        const n = Number(this.maxLookbehindField.value);
+        if (n > 0) {
+            this.markov.maxLookbehind = n;
+        } else {
+            this.markov.maxLookbehind = Infinity;
+        }
+    }
+    handleLearningModeSelectChange() {
+        if (this.busy) {
+            this.setLearningModeInUIFromModel();
+        } else {
+            this.setLearningModeInModelFromUI();
+        }
+    }
+    setLearningModeInUIFromModel() {
+        this.learningModeSelect.value = this.learningMode;
+    }
+    setLearningModeInModelFromUI() {
+        this.learningMode = this.learningModeSelect.value;
+    }
+    handleTokenModeSelectChange() {
+        if (this.busy) {
+            this.setTokenModeInUIFromModel();
+        } else {
+            this.setTokenModeInModelFromUI();
+        }
+    }
+    setTokenModeInUIFromModel() {
+        this.tokenModeSelect.value = this.markov.mode;
+    }
+    setTokenModeInModelFromUI() {
+        this.markov.mode = this.tokenModeSelect.value;
+    }
+    async handleExportButtonClick() {
+        if (!this.busy) {
+            this.disable();
+            await Markov.DataIO.compressAndSave(
+                this.exportToJSON()
+            );
+            this.enable();
+        }
+    }
+    async handleImportButtonChange() {
+        if (!this.busy) {
+            this.disable();
+            this.importFromJSON(
+                await Markov.DataIO.loadAndDecompress(
+                    this.importButton.files[0]
+                )
+            );
+            this.enable();
+            this.promptField.focus();
+        }
+    }
+    async respond(prompt) {
+        if (!this.busy) {
+            this.disable();
+            const prompt = this.promptField.value;
+            this.promptField.value = "";
+            const ui = this;
+            const coro = new Coroutine(async function () {
+                switch (ui.learningMode) {
+                    case "conversational": {
+                        await ui.markov.learn(
+                            ui.responseArea.innerText,
+                            prompt, this
+                        );
+                    } break;
+                    case "instructive": {
+                        await ui.markov.learn(
+                            ui.lastPrompt,
+                            prompt, this
+                        );
+                    } break;
+                    default: {
+                        await this.yield(0);
+                    } break;
+                }
+                await this.yield("|||");
+                await ui.markov.respond(prompt, this);
+            });
             const learnSteps = await coro.resume();
             for await (const value of coro) {
                 if (value == "|||") {
                     break;
                 } else {
-                    outputArea.innerText =
+                    this.responseArea.innerText =
                         `Learning... ${Math.round(value*100/learnSteps)}%`;
-                    await yieldToPage();
+                    await this.yieldToPage();
                 }
             }
-            outputArea.innerText = "";
             let str = "";
             for await (const value of coro) {
-                str = markov.detokenize([str, value]);
-                outputArea.innerText = str;
-                await yieldToPage();
+                str = this.markov.detokenize([str, value]);
+                this.responseArea.innerText = str;
+                await this.yieldToPage();
             }
-            lastKnownBrain = brainArea.value = markov.externAssociations();
-        } catch (err) {
-            console.log(err);
-            outputArea.innerText = `
-                [Markov encountered an internal error.
-                This may result from model-incompatible changes
-                to its association table (its "brain").
-                See browser developer console for error details.]
-            `.split(/\s+/).filter(s => s != "").join(" ");
-            errorState = true;
-            markov.reset();
-            markov.internAssociations(brainArea.value);
-        } finally {
-            busy = false;
-            brainArea.disabled = false;
+            this.lastPrompt = prompt;
+            this.enable();
+            this.promptField.focus();
         }
     }
-});
-
-brainArea.addEventListener("change", () => {
-    const text = (brainArea.value == "") ? "{}" : brainArea.value;
-    if (
-        !busy &&
-        text != lastKnownBrain &&
-        markov.internAssociations(text)
-    ) {
-        lastKnownBrain = text;
+    exportToJSON() {
+        return JSON.stringify({
+            lastPrompt: this.lastPrompt,
+            //lastResponse: this.responseArea.innerText,
+            //learningMode: this.learningMode,
+            tokenMode: this.markov.mode,
+            maxLookbehind: this.markov.maxLookbehind,
+            associations: this.markov.associations
+        });
     }
-});
+    importFromJSON(json) {
+        const obj = JSON.parse(json);
+        this.lastPrompt = obj.lastPrompt;
+        //this.responseArea.innerText = obj.lastResponse;
+        //this.learningMode = obj.learningMode;
+        this.markov.mode = obj.tokenMode;
+        this.markov.maxLookbehind = obj.maxLookbehind;
+        this.markov.associations = obj.associations;
+        this.responseArea.innerText = "";
+        this.lastResponse = "";
+        this.learningMode = "disabled";
+        this.syncUIFromModel();
+    }
+    async handleCollectionImportButtonClick(id) {
+        if (!this.busy) {
+            this.disable();
+            this.importFromJSON(
+                await Markov.DataIO.fetchAndDecompress(
+                    `./collection/${id}.gz`
+                )
+            );
+            this.enable();
+            this.promptField.focus();
+        }
+    }
+    syncUIFromModel() {
+        this.setMaxLookbehindInUIFromModel();
+        this.setLearningModeInUIFromModel();
+        this.setTokenModeInUIFromModel();
+    }
+    syncModelFromUI() {
+        this.setMaxLookbehindInModelFromUI();
+        this.setLearningModeInModelFromUI();
+        this.setTokenModeInModelFromUI();
+    }
+};
+
+new (Markov.UI)();
+
+window.Markov = Markov;
